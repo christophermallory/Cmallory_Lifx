a851e8150cb601ec6dea87be55c001de98c17565
Chris Mallory <chris@christophermallory.com>
2020-12-27 15:36:56 -0700

    Update comments on observer.php
    Update comments to be more accurate - remove specifying the order total of $2500 for orders since this is configurable.

--- app/code/local/Cmallory/Lifx/Model/Observer.php
+++ app/code/local/Cmallory/Lifx/Model/Observer.php
@@ -3,15 +3,15 @@ class Cmallory_Lifx_Model_Observer
 {
     public function sendSalesLight(Varien_Event_Observer $observer)
     {
-        // Check if order is over $2500
-      $invoice = $observer->getEvent()->getInvoice();
+        // Check order value
+        $invoice = $observer->getEvent()->getInvoice();
         $order = $invoice->getOrder();
         $orderNo = $order->getIncrementID();
         $subtotal = $order->getSubtotal();
         $reqSubtotal = Mage::getStoreConfig('cmallory_lifx/general/subtotal', Mage::app()->getStore());
         if ($subtotal > $reqSubtotal) {
             // Send request to LIFX API
-          $link = "https://api.lifx.com/v1/lights/all/effects/breathe";
+            $link = "https://api.lifx.com/v1/lights/all/effects/breathe";
             $authToken = Mage::getStoreConfig('cmallory_lifx/general/api_token', Mage::app()->getStore());
             $headers = array('Authorization: Bearer ' . $authToken);
             $data = 'period=' . Mage::getStoreConfig('cmallory_lifx/general/period', Mage::app()->getStore()) . '&cycles=' . Mage::getStoreConfig('cmallory_lifx/general/flash', Mage::app()->getStore()) . '&color=' . Mage::getStoreConfig('cmallory_lifx/general/color', Mage::app()->getStore());
